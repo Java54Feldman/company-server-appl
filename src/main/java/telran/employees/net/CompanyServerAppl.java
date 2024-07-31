6c4378caaabf26951c6cb1108e4785cc5d0b2e36
Pavel Feldman <feldmanpk@gmail.com>
2024-07-31 17:15:03 +0300

    HW #41 added multi-threading & shutdown TcpServer

--- src/main/java/telran/employees/net/CompanyServerAppl.java
+++ src/main/java/telran/employees/net/CompanyServerAppl.java
@@ -1,6 +1,7 @@
 package telran.employees.net;
 
 
+import java.io.*;
 import telran.employees.*;
 import telran.io.Persistable;
 import telran.net.*;
@@ -19,13 +20,29 @@ public class CompanyServerAppl {
 		}
 		Protocol protocol = new CompanyProtocol(company);
 		TcpServer tcpServer = new TcpServer(protocol, PORT);
-		//FIXME need to start TCPServer as a thread
-		tcpServer.run();
-		//TODO
+		//start TcpServer as a thread
+		Thread serverThread = new Thread(tcpServer);
+		serverThread.start();
 		//cycle with asking a user to enter shutdown for exit from the server
 		//regular while cycle with no using cli-view
 		//by entering "shutdown" you should call method shutdown of the TcpServer
 		//after shutdown you should perform saving the data into the file
-	}
+		BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
+        while (true) {
+            System.out.println("Enter 'shutdown' to stop the server");
+            try {
+				String input = reader.readLine();
+				if ("shutdown".equalsIgnoreCase(input)) {
+				    tcpServer.shutdown();
+				    ((Persistable)company).save(FILE_NAME);
+				    System.out.println("Server saved data and closed connection");
+				    break;
+				}
+			} catch (Exception e) {
+				System.out.println("Error saving data: " + e.getMessage());
+			}
+        }
 
+	}
+	
 }
