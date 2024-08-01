b10de26d52b5fa7f166b2aeffdd271b75e90f625
Pavel Feldman <feldmanpk@gmail.com>
2024-08-01 18:14:53 +0300

    HW #41 Yuri solution

--- src/main/java/telran/employees/net/CompanyServerAppl.java
+++ src/main/java/telran/employees/net/CompanyServerAppl.java
@@ -1,10 +1,12 @@
 package telran.employees.net;
 
 
-import java.io.*;
+import java.util.Scanner;
+
 import telran.employees.*;
 import telran.io.Persistable;
 import telran.net.*;
+import static telran.net.TcpConfigurationProperties.*;
 
 public class CompanyServerAppl {
 
@@ -13,35 +15,45 @@ public class CompanyServerAppl {
 
 	public static void main(String[] args) {
 		Company company = new CompanyMapsImpl();
-		try {
-			((Persistable)company).restore(FILE_NAME);
-		} catch (Exception e) {
-
+		Persistable persistable = null;
+		if(company instanceof Persistable) {
+			persistable = (Persistable)company;
 		}
+		if (persistable != null) {
+			try {
+				persistable.restore(FILE_NAME);
+			} catch (Exception e) {
+
+			} 
+		}
+		
+		//cycle with asking a user to enter shutdown for exit from the server
+		//regular while cycle with no using cli-view
+		//by entering "shutdown" you should call method shutdown of the TcpServer
+		//after shutdown you should perform saving the data into the file
 		Protocol protocol = new CompanyProtocol(company);
 		TcpServer tcpServer = new TcpServer(protocol, PORT);
 		//start TcpServer as a thread
 		Thread serverThread = new Thread(tcpServer);
 		serverThread.start();
-		//cycle with asking a user to enter shutdown for exit from the server
-		//regular while cycle with no using cli-view
-		//by entering "shutdown" you should call method shutdown of the TcpServer
-		//after shutdown you should perform saving the data into the file
-		BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
-        while (true) {
-            System.out.println("Enter 'shutdown' to stop the server");
-            try {
-				String input = reader.readLine();
-				if ("shutdown".equalsIgnoreCase(input)) {
-				    tcpServer.shutdown();
-				    ((Persistable)company).save(FILE_NAME);
-				    System.out.println("Server saved data and closed connection");
-				    break;
+		boolean running = true;
+		try (Scanner scanner = new Scanner(System.in);){
+			while(running) {
+				System.out.println(String.format("Enter %s for the server shutdown", SHUTDOWN));
+				String line = scanner.nextLine();
+				if (line != null && line.equals(SHUTDOWN)) {
+					running = false;
+					tcpServer.shutdown();
+					if (persistable != null) {
+						try {
+							persistable.save(FILE_NAME);
+						} catch (Exception e) {
+
+						}
+					}
 				}
-			} catch (Exception e) {
-				System.out.println("Error saving data: " + e.getMessage());
 			}
-        }
+		}
 
 	}
 	
